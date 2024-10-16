8b20aafe529284c893384d0c782377f899f839b1
anup015 <167200406+anup015@users.noreply.github.com>
2024-10-16 14:49:07 +0530

    Update IOOP_Main.cpp
    user class definition

--- IOOP_Main.cpp
+++ IOOP_Main.cpp
@@ -1,1 +1,20 @@
-
+#include<iostream>
+#include<fstream>
+#include<cstring>
+using namespace std;
+class user{
+    public:
+    string name,email,password;
+    user(){
+        cout<<"Enter name : ";
+        cin>>name;
+        cout<<"Enter email : ";
+        cin>>email;
+        cout<<"Enter password : ";
+        cin>>password;
+    }
+    void show_details(){
+        cout<<"Name : "<<name<<endl;
+        cout<<"Email : "<<email<<endl;
+    }
+};
