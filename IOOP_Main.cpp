c4f58898dd8cdc339c0a1fe8b1b750148c45c2c5
shivang tonde <shivangtonde1811@shivangs-MacBook-Air.local>
2024-10-16 17:12:55 +0530

    Some changes in User

--- IOOP_Main.cpp
+++ IOOP_Main.cpp
@@ -1,24 +1,40 @@
-#include<iostream>
-#include<fstream>
-#include<cstring>
+#include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
-class user{
-    public:
-    string name,email,password;
-    user(){
-        cout<<"Enter name : ";
-        cin>>name;
-        cout<<"Enter email : ";
-        cin>>email;
-        cout<<"Enter password : ";
-        cin>>password;
+
+class user
+{
+public:
+    string name, email, password, role; // role is used only when object is of inherited class.
+    int userID;
+    static int userCount;
+
+    user()
+    {
+        cout << "Enter name : ";
+        cin >> name;
+        cout << "Enter email : ";
+        cin >> email;
+        cout << "Enter password : ";
+        cin >> password;
+        userID = ++userCount;
     }
-    void show_details(){
-        cout<<"Name : "<<name<<endl;
-        cout<<"Email : "<<email<<endl;
+    user(string name, string email, string password, string role) : name(name), email(email), password(password), role(role)
+    {
+        userID = userCount++;
+    }
+    void show_details()
+    {
+        cout << "Name : " << name << endl;
+        cout << "Email : " << email << endl;
     }
 };
-class buyer:public user{
-    public:
-        string list;
+
+int user::userCount = 18007;
+class buyer : public user
+{
+public:
+    string list;
 };
